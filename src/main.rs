99d21673bb8ac3ea8c2ae2f12ae04893a69c3e54
Mathias Rav <m@git.strova.dk>
2018-10-14 20:50:30 +0200

    Initial commit of fbspinner code

---
+++ src/main.rs
@@ -0,0 +1,93 @@
+extern crate flate2;
+extern crate libc;
+
+mod framebuffer;
+
+use std::{io, fmt, result, fs, thread, time};
+use std::io::Read;
+//use std::io::prelude::*;
+use flate2::read::ZlibDecoder;
+
+#[derive(Debug)]
+pub enum ErrorKind {
+    CompressionUnknown(u32),
+    Io(io::Error),
+    UnexpectedEof,
+    VersionIsZero,
+}
+
+#[derive(Debug)]
+pub struct Error {
+    kind: ErrorKind,
+}
+
+pub type Result<T> = result::Result<T, Error>;
+
+impl Into<Error> for ErrorKind {
+    fn into(self) -> Error {
+        Error { kind: self }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        if e.kind() == io::ErrorKind::UnexpectedEof {
+            ErrorKind::UnexpectedEof.into()
+        } else {
+            ErrorKind::Io(e).into()
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.kind {
+            ErrorKind::CompressionUnknown(c) => write!(f, "Unknown compression {}", c),
+            ErrorKind::Io(ref e) => write!(f, "{}", e),
+            ErrorKind::UnexpectedEof => write!(f, "Unexpected end-of-file"),
+            ErrorKind::VersionIsZero => write!(f, "Version is 0"),
+        }
+    }
+}
+
+fn read_u32<R: io::Read>(r: &mut R) -> Result<u32> {
+    let buf = &mut [0, 0, 0, 0];
+    r.read_exact(buf)?;
+    Ok(
+        ((buf[3] as u32) << 24) + ((buf[2] as u32) << 16) + ((buf[1] as u32) << 8)
+            + (buf[0] as u32),
+    )
+}
+
+// fn read_u32_or_eof<R: io::Read>(r: &mut R) -> Result<Option<u32>> {
+//     match read_u32(r) {
+//         Ok(v) => Ok(Some(v)),
+//         Err(e) => match e.kind {
+//             ErrorKind::UnexpectedEof => Ok(None),
+//             _ => Err(e),
+//         },
+//     }
+// }
+
+fn main() {
+    let mut animdata = fs::File::open("anim.bin").unwrap();
+    let nframes = read_u32(&mut animdata).unwrap() as usize;
+    let height = read_u32(&mut animdata).unwrap() as usize;
+    let width = read_u32(&mut animdata).unwrap() as usize;
+    let bpp = read_u32(&mut animdata).unwrap() as usize;
+    let frame_size = height*width*bpp;
+    let mut frames = vec![0; nframes*frame_size];
+    let mut decoder = ZlibDecoder::new(animdata);
+    decoder.read_exact(&mut frames).unwrap();
+    assert_eq!(0, decoder.read(&mut [0]).unwrap());
+
+    let mut fb = framebuffer::Framebuffer::new("/dev/fb0");
+    let mut writer = fb.writer(width, height);
+    let dur = time::Duration::from_millis(1000 / 30);
+    loop {
+        for i in 0..nframes {
+            writer.write(&frames[i * frame_size..(i + 1) * frame_size]).unwrap();
+            thread::sleep(dur);
+        }
+    }
+}
